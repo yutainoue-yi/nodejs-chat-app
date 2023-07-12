7b94fd3430b92389270fbea776c8d91be52a78cf
yutainoue-yi <yuta6ace@gmail.com>
2023-07-12 22:34:19 +0900

    first push

---
+++ server.js
@@ -0,0 +1,28 @@
+//expressを読み込む
+const express = require("express");
+//使用できる形へ
+const app = express();
+const http = require("http");
+const { Socket } = require("socket.io");
+//サーバーを作成
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+io.on("connection", (socket) => {
+    console.log("ユーザーが接続しました。");
+
+    socket.on("chat message", (msg) => {
+        //console.log("メッセージ:" + msg);
+        io.emit("chat message", msg);
+    });
+});
+
+//サーバーを立ち上げる
+server.listen(PORT, () => {
+    console.log("listening on 3000");
+})
